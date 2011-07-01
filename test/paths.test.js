d44d03e3e390b2998c9b49926c52f6dd0a260e7f
Michael Williamson <mike@zwobble.org>
2011-07-01 23:15:43 +0100

    Change end to accept an optional required parameter

--- test/paths.test.js
+++ test/paths.test.js
@@ -2,24 +2,20 @@ var paths = require("blog/paths");
 
 exports.pathEndingAtRootMatchesSingleSlash  = function(test) {
     var navigator = paths.navigator(
-        paths.end(function(parameters, callback) {
-            callback(null, "root");
-        })
+        paths.end()
     );
     navigator.navigate("/", function(err, result) {
-        test.equal("root", result);
+        test.ok(result.matched);
         test.done();
     });
 };
 
 exports.pathEndingAtRootDoesntMatchPathOtherThanSingleSlash  = function(test) {
     var navigator = paths.navigator(
-        paths.end(function(parameters, callback) {
-            callback(null, "root");
-        })
+        paths.end()
     );
     navigator.navigate("/blogs", function(err, result) {
-        test.equal(null, result);
+        test.ok(!result.matched);
         test.done();
     });
 };
@@ -28,16 +24,13 @@ exports.regexPathMatcherAddsComponentToParameters  = function(test) {
     var digitParameter = paths.parameters.regex(/[0-9]+/);
     var navigator = paths.navigator(
         paths.then(digitParameter,
-            paths.end(function(parameters, callback) {
-                callback(null, parameters);
-            })
+            paths.end(digitParameter)
         )
     );
-    navigator.navigate("/2351", function(err, parameters) {
-        parameters.get(digitParameter, function(err, digit) {
-            test.equal("2351", digit);
-            test.done();
-        });
+    navigator.navigate("/2351", function(err, result) {
+        test.ok(result.matched);
+        test.equal("2351", result.value);
+        test.done();
     });
 };
 
@@ -45,39 +38,35 @@ exports.regexPathMatcherDoesntMatchStringsNotMatchingRegex  = function(test) {
     var digitParameter = paths.parameters.regex(/[0-9]+/);
     var navigator = paths.navigator(
         paths.then(digitParameter,
-            paths.end(function(parameters, callback) {
-                callback(null, parameters);
-            })
+            paths.end(digitParameter)
         )
     );
     navigator.navigate("/rock-and-roll", function(err, result) {
-        test.equal(null, result)
+        test.ok(!result.matched);
         test.done();
     });
 };
 
 exports.thenTriesMatchersInOrderUntilSuccess  = function(test) {
+    var firstParameter = paths.parameters.regex(/1/);
+    var secondParameter = paths.parameters.regex(/2/);
+    var thirdParameter = paths.parameters.regex(/2/);
     var navigator = paths.navigator(
         paths.firstOf(
-            paths.then(paths.parameters.regex(/1/),
-                paths.end(function(parameters, callback) {
-                    callback(null, "1");
-                })
+            paths.then(firstParameter,
+                paths.end()
             ),
-            paths.then(paths.parameters.regex(/2/),
-                paths.end(function(parameters, callback) {
-                    callback(null, "2");
-                })
+            paths.then(secondParameter,
+                paths.end(secondParameter)
             ),
-            paths.then(paths.parameters.regex(/2/),
-                paths.end(function(parameters, callback) {
-                    callback(null, "3");
-                })
+            paths.then(thirdParameter,
+                paths.end()
             )
         )
     );
     navigator.navigate("/2", function(err, result) {
-        test.equal("2", result)
+        test.ok(result.matched);
+        test.equal("2", result.value)
         test.done();
     });
 };
@@ -88,16 +77,13 @@ exports.canConvertValuesFromParameters  = function(test) {
     });
     var navigator = paths.navigator(
         paths.then(digitParameter,
-            paths.end(function(parameters, callback) {
-                callback(null, parameters);
-            })
+            paths.end(digitParameter)
         )
     );
-    navigator.navigate("/23", function(err, parameters) {
-        parameters.get(digitParameter, function(err, digit) {
-            test.equal(46, digit);
-            test.done();
-        });
+    navigator.navigate("/23", function(err, result) {
+        test.ok(result.matched);
+        test.equal(46, result.value);
+        test.done();
     });
 };
 
@@ -107,13 +93,26 @@ exports.valuesNotConvertedIfParameterDoesntMatch  = function(test) {
     });
     var navigator = paths.navigator(
         paths.then(digitParameter,
-            paths.end(function(parameters, callback) {
-                callback(null, parameters);
-            })
+            paths.end()
         )
     );
     navigator.navigate("/23a", function(err, result) {
-        test.equal(null, result)
+        test.ok(!result.matched);
+        test.done();
+    });
+};
+
+exports.conversionErrorsArePassedBack  = function(test) {
+    var digitParameter = paths.parameters.convert(paths.parameters.regex(/[0-9]+/), function(value, callback) {
+        callback(new Error("Oh noes"));
+    });
+    var navigator = paths.navigator(
+        paths.then(digitParameter,
+            paths.end(digitParameter)
+        )
+    );
+    navigator.navigate("/23", function(err) {
+        test.equal("Oh noes", err.message);
         test.done();
     });
 };
@@ -121,13 +120,11 @@ exports.valuesNotConvertedIfParameterDoesntMatch  = function(test) {
 exports.stringPathMatcherOnlyMatchesExactlyTheSameString = function(test) {
     var navigator = paths.navigator(
         paths.then("blog",
-            paths.end(function(parameters, callback) {
-                callback(null, "hooray!");
-            })
+            paths.end()
         )
     );
     navigator.navigate("/blog", function(err, result) {
-        test.equal("hooray!", result)
+        test.ok(result.matched);
         test.done();
     });
 };
@@ -135,13 +132,11 @@ exports.stringPathMatcherOnlyMatchesExactlyTheSameString = function(test) {
 exports.stringPathMatcherDoesntMatchDifferentString = function(test) {
     var navigator = paths.navigator(
         paths.then("blog",
-            paths.end(function(parameters, callback) {
-                callback(null, "hooray!");
-            })
+            paths.end()
         )
     );
     navigator.navigate("/Blog", function(err, result) {
-        test.equal(null, result)
+        test.ok(!result.matched);
         test.done();
     });
 };
@@ -151,29 +146,80 @@ exports.canComposeParameters = function(test) {
     var monthParameter = paths.parameters.regex(/[0-9]+/);
     var dayParameter = paths.parameters.regex(/[0-9]+/);
     
-    var dateParameter = paths.parameters.composite(function(parameters, callback) {
-        parameters.getAll(yearParameter, monthParameter, dayParameter, function(err, year, month, day) {
-            callback(err, {year: year, month: month, day: day});
-        });
-    });
+    var dateParameter = paths.parameters.composite(
+        yearParameter, monthParameter, dayParameter,
+        function(year, month, day, callback) {
+            callback(null, {matched: true, value: {year: year, month: month, day: day}});
+        }
+    );
     
     var navigator = paths.navigator(
         paths.then(yearParameter,
             paths.then(monthParameter,
                 paths.then(dayParameter,
-                    paths.end(function(parameters, callback) {
-                        callback(null, parameters);
-                    })
+                    paths.end(dateParameter)
                 )
             )
         )
     );
-    navigator.navigate("/2011/06/30/", function(err, parameters) {
-        parameters.get(dateParameter, function(err, date) {
-            test.equal("2011", date.year);
-            test.equal("06", date.month);
-            test.equal("30", date.day);
-            test.done();
-        });
+    navigator.navigate("/2011/06/30/", function(err, result) {
+        test.ok(result.matched);
+        var date = result.value;
+        test.equal("2011", date.year);
+        test.equal("06", date.month);
+        test.equal("30", date.day);
+        test.done();
     });
 };
+
+exports.errorsFromCompositionArePassedBack = function(test) {
+    var yearParameter = paths.parameters.regex(/[0-9]+/);
+    var monthParameter = paths.parameters.regex(/[0-9]+/);
+    var dayParameter = paths.parameters.regex(/[0-9]+/);
+    
+    
+    var dateParameter = paths.parameters.composite(
+        yearParameter, monthParameter, dayParameter,
+        function(year, month, day, callback) {
+            callback(new Error("Oh noes"));
+        }
+    );
+    
+    var navigator = paths.navigator(
+        paths.then(yearParameter,
+            paths.then(monthParameter,
+                paths.then(dayParameter,
+                    paths.end(dateParameter)
+                )
+            )
+        )
+    );
+    navigator.navigate("/2011/06/30/", function(err, result) {
+        test.equal("Oh noes", err.message);
+        test.done();
+    });
+};
+
+exports.notMatchedIfDependencyOfCompositionIsUnavailable = function(test) {
+    var yearParameter = paths.parameters.regex(/[0-9]+/);
+    var monthParameter = paths.parameters.regex(/[0-9]+/);
+    var dayParameter = paths.parameters.regex(/[0-9]+/);
+    
+    var dateParameter = paths.parameters.composite(
+        yearParameter, monthParameter, dayParameter,
+        function(year, month, day, callback) {
+            callback(null, {matched: true, value: {year: year, month: month, day: day}});
+        }
+    );
+    
+    var navigator = paths.navigator(
+        paths.then(yearParameter,
+            paths.end(dateParameter)
+        )
+    );
+    navigator.navigate("/2011", function(err, result) {
+        test.ok(!result.matched);
+        test.done();
+    });
+};
+
