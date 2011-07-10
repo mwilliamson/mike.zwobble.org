774a921123e65867612416837a338ab3f7557c3b
Michael Williamson <mike@zwobble.org>
2011-07-10 21:56:27 +0100

    Add basic authentiation to HTTPS

--- templates/settings.example.js
+++ templates/settings.example.js
@@ -8,3 +8,8 @@ exports.httpPort = 8125;
 exports.httpsPort = 8126;
 exports.tlsKeyPath = "server.key";
 exports.tlsCertPath = "server.crt";
+
+exports.auth = {
+    username: "admin",
+    hash: "$2a$10$9GFeHTZm6PKprCV0pUlHMuZ6p8oWIIyspbIM.fKrYO9S0c3kGSViy"
+};
