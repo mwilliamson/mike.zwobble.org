7d18b25f0b0dcd8f29bcc4a26f63c886c7d84636
Michael Williamson <mike@zwobble.org>
2011-07-14 19:19:55 +0100

    Use username and password for mongo properly

--- test/mongo.test.js
+++ test/mongo.test.js
@@ -1,9 +1,11 @@
 var mongo = require("blog/mongo");
 
 exports.canParseMongoUri = function(test) {
-    var uri = "mongodb://username:password@host:478/database";
+    var uri = "mongodb://bob:blah@host:478/database";
     var result = mongo.parseUri(uri);
-    test.equal("username:password@host", result.host);
+    test.equal("bob", result.username);
+    test.equal("blah", result.password);
+    test.equal("host", result.host);
     test.equal("478", result.port);
     test.equal("database", result.database);
     test.done();
